54fb26d2bed294837dc82fdf3272ef537b954044
marcelo macedo <marcelo3macedo@gmail.com>
2019-02-21 22:53:14 -0300

    Added Filter Date

--- api/objects/tickets.php
+++ api/objects/tickets.php
@@ -10,11 +10,12 @@ class Ticket {
         $this->conn = $db;
     }
 
-    function readToJson($ordering){ 
+    function readToJson($ordering, $start, $end){ 
         $order = $this->getOrderBy($ordering);
+        $filter = $this->getFilters($start, $end);
         $response=array();
 
-        $query = "SELECT * FROM " . $this->table_name . " " . $order;
+        $query = "SELECT * FROM " . $this->table_name . " " . $filter . " " . $order;
 
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
@@ -45,6 +46,32 @@ class Ticket {
         }   
         
         return "";
+    }
+
+    function getFilters($start, $end) {
+        $filter = "";
+
+        if ($this->checkValidDate($start)) {
+            $filter = "WHERE DateCreate > '" . $start . "'";
+        }
+
+        if ($this->checkValidDate($end)) {
+            if (strlen($filter) > 0) {
+                $filter = $filter . " AND DateCreate < '" . $end . "'";
+            } else {
+                $filter = $filter . " WHERE DateCreate < '" . $end . "'";
+            }            
+        }
+
+        return $filter;
+    }
+ 
+    function checkValidDate($date) {
+        if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$date)) {
+            return true;
+        }
+
+        return false;
     }
 }
 ?>
--- api/read.php
+++ api/read.php
@@ -10,15 +10,25 @@ $database = new Database();
 $db = $database->getConnection();
 
 $ordering = "";
+$start = "";
+$end = "";
 
 if (isset($_GET['order'])) {
     $ordering = $_GET['order'];
 }
 
+if (isset($_GET['start'])) {
+    $start = $_GET['start'];
+}
+
+if (isset($_GET['end'])) {
+    $end = $_GET['end'];
+}
+
 $ticket = new Ticket($db);
 $interaction = new Interaction($db);
 
-$tickets = $ticket->readToJson($ordering);
+$tickets = $ticket->readToJson($ordering, $start, $end);
 
 for ($x = 0; $x < count($tickets); $x++) {
     $tickets[$x]["Interactions"] = $interaction->getByTicket($tickets[$x]["TicketID"]);
